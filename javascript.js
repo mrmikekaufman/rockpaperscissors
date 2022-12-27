719c370aea1042a6b35ff6177923d14186338edd
Michael Kaufman <mrmikekaufman@gmail.com>
2022-12-27 10:22:19 -0800

    Create if else statement to determine winner
    Use if else statement to compare user input to random computer choice.
    Go through all possible scenarios to determine winner and print out
    statement declaring who won.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 //Create function (getComputerChoice) that randomly gives back the computers choice
 
 //Prompt user for their choice
+let playerSelection = prompt('What will you choose?', 'Rock? Paper? Scissors?');
+
 
 //For loop that loops the game 5 times
 
