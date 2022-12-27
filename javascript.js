3edd299cd242b5357ab41c094d51d071201cbc3a
Michael Kaufman <mrmikekaufman@gmail.com>
2022-12-27 11:21:49 -0800

    Integrate all functions in to game functions
    Use getComputerChoice and playRound functions within
    game() function to complete game. For loop runs game 5
    times. Score for computer and user are tallied to determine
    winner.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,74 @@
 //Create function (getComputerChoice) that randomly gives back the computers choice
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    let randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
 
-//Prompt user for their choice
-let playerSelection = prompt('What will you choose?', 'Rock? Paper? Scissors?');
+//initialize variables for player score and computer score.
+let playerScore = 0;
+let computerScore = 0;
+//If else statements comparing computer choice to user choice (rock beats scissors, etc.). Include in function
+//called playRound.
 
+function playRound(playerSelection, computerSelection) {
 
-//For loop that loops the game 5 times
+    if (playerSelection === computerSelection) {
+        return "Wow! It\'s a tie! Let\'s try again";
+    }
+    else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            computerScore++;
+            return 'Oh no! Paper beats Rock. The computer wins. Try again!';
+        }
+        else if (computerSelection === 'scissors') {
+            playerScore++;
+            return 'Yes! You win. Rock beats Scissors.';
+        }
+    }
+    else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            playerScore++;
+            return 'Yes! Paper beats Rock. You win!';
+        }
+        else if (computerSelection === 'scissors') {
+            computerScore++;
+            return 'Oh no! Scissors cuts paper. You lose.';
+        }
+    }
+    else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            computerScore++;
+            return 'Rock beats Scissors. You lose.';
+        }
+        else if (computerSelection === 'paper') {
+            playerScore++;
+            return 'Scissors slices Paper. You are a winner!';
+        }
+    }
+}
 
-//If else statements comparing computer choice to user choice (rock beats scissors, etc.)
+//game() function -> calls playRound and declares a winner. For loop that loops the game 5 times
+function game() {
+    for (let i = 0; i < 5; i++) {
+        //Prompt user for their choice
+        let playerResponse = prompt('What will you choose?', 'Rock? Paper? Scissors?');
+        let playerSelection = playerResponse;
+        //Get random computer selection
+        const computerSelection = getComputerChoice();
+        //Play 5 rounds returning who won after each round
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    //Compare computer score to player score after 5 rounds to determine a final winner.
+    if (playerScore > computerScore) {
+        console.log('You win! Congrats.');
+    }
+    else if (computerScore > playerScore) {
+        console.log('You lose. Try again');
+    }
+    else {
+        console.log("It's a tie!");
+    }
+}
 
-//Console.log displaying winner
-
-//game() function -> calls playRound and declares a winner.
-
+game();
