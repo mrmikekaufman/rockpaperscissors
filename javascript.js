96c1182edd88db423384a7f48ec21598d3b2880d
Michael Kaufman <mrmikekaufman@gmail.com>
2022-12-27 10:04:22 -0800

    Outline pseudocode for rockpaperscissors game
    Create html file and javascript file. Use comments in javascript file
    to outline pseudocode for rock paper scissors game.

---
+++ javascript.js
@@ -0,0 +1,12 @@
+//Create function (getComputerChoice) that randomly gives back the computers choice
+
+//Prompt user for their choice
+
+//For loop that loops the game 5 times
+
+//If else statements comparing computer choice to user choice (rock beats scissors, etc.)
+
+//Console.log displaying winner
+
+//game() function -> calls playRound and declares a winner.
+
