81f72a654403b2110fe41878a84b2ef8d1112a32
Michael Kaufman <mrmikekaufman@gmail.com>
2023-01-04 12:30:16 -0800

    Create score divs and declare winner of game
    Divs are created holding information about game when clicking buttons.
    When computer or user gets to 5 points a winner will be declared and the
    game will reset. Window confirm will appear which if clicked
    player and computer score will go back to 0. User can still continue to
    play even if clicking cancel on window.

--- javascript.js
+++ javascript.js
@@ -5,80 +5,83 @@ function getComputerChoice() {
     return choices[randomNumber];
 }
 
+const buttons = document.querySelectorAll('button');
+const results = document.querySelector('.results')
+const declareWinner = document.createElement('div');
+const scoreboard = document.createElement('div');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const computerSelection = getComputerChoice();
+        playRound(button.className, computerSelection);  
+    });
+});
+
 //initialize variables for player score and computer score.
 let playerScore = 0;
 let computerScore = 0;
 //If else statements comparing computer choice to user choice (rock beats scissors, etc.). Include in function
 //called playRound.
 
-function playRound(playerSelection, computerSelection) {
+function reset (winner) {
+    let winningMessage = ''
+    if (winner === 'computer'){
+        winningMessage = 'Oh no! You let the computer win. Would you like to play again?';
+    }
+    else {
+        winningMessage = 'Wohoo! You won. Would you like to play again?';
+    }
+    if(window.confirm(`${winningMessage}`))
+    {
+        results.textContent = 'Click to start New Game';
+        results.removeChild(scoreboard);
+        results.removeChild(declareWinner)
+    };
+    playerScore = 0;
+    computerScore = 0;
+}
 
+function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return "Wow! It\'s a tie! Let\'s try again";
+        results.textContent = "Wow! It\'s a tie! Let\'s try again";
     }
     else if (playerSelection === 'rock') {
         if (computerSelection === 'paper') {
             computerScore++;
-            return 'Oh no! Paper beats Rock. The computer wins. Try again!';
+            results.textContent = 'Oh no! Paper beats Rock. The computer wins. Try again!';
         }
         else if (computerSelection === 'scissors') {
             playerScore++;
-            return 'Yes! You win. Rock beats Scissors.';
+            results.textContent = 'Yes! You win. Rock beats Scissors.';
         }
     }
     else if (playerSelection === 'paper') {
         if (computerSelection === 'rock') {
             playerScore++;
-            return 'Yes! Paper beats Rock. You win!';
+            results.textContent = 'Yes! Paper beats Rock. You win!';
         }
         else if (computerSelection === 'scissors') {
             computerScore++;
-            return 'Oh no! Scissors cuts paper. You lose.';
+            results.textContent = 'Oh no! Scissors cuts paper. You lose.';
         }
     }
     else if (playerSelection === 'scissors') {
         if (computerSelection === 'rock') {
             computerScore++;
-            return 'Rock beats Scissors. You lose.';
+            results.textContent = 'Rock beats Scissors. You lose.';
         }
         else if (computerSelection === 'paper') {
             playerScore++;
-            return 'Scissors slices Paper. You are a winner!';
+            results.textContent = 'Scissors slices Paper. You are a winner!';
         }
     }
+    scoreboard.textContent = `Computer Score: ${computerScore} Player Score: ${playerScore}`;
+    results.appendChild(scoreboard);
+    if (playerScore === 5) {
+        reset('player');
+    } else if (computerScore === 5) {
+        reset('computer');
+    }
+
 }
 
-//add event listeners for each button that calls playRound w/ correct player selection
-const buttons = document.querySelectorAll('button');
-const computerSelection = getComputerChoice();
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        console.log(playRound(button.className, computerSelection));
-    });
-});
-
-
-
-//game() function -> calls playRound and declares a winner. For loop that loops the game 5 times
-function game() {
-    //Prompt user for their choice
-    // let playerResponse = prompt('What will you choose?', 'Rock? Paper? Scissors?');
-    let playerSelection = playerResponse;
-    //Get random computer selection
-    const computerSelection = getComputerChoice();
-    //Play 5 rounds returning who won after each round
-    console.log(playRound(playerSelection, computerSelection));
-
-    //Compare computer score to player score after 5 rounds to determine a final winner.
-    if (playerScore > computerScore) {
-        console.log('You win! Congrats.');
-    }
-    else if (computerScore > playerScore) {
-        console.log('You lose. Try again');
-    }
-    else {
-        console.log("It's a tie!");
-    }
-}
-
-// game();
